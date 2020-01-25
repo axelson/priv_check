b19800834ff57b0066d989279c6c80d8171711ee
Jason Axelson <jason.axelson@gmail.com>
2020-01-25 06:21:53 -1000

    Initial commit

---
+++ lib/priv_check.ex
@@ -0,0 +1,18 @@
+defmodule PrivCheck do
+  @moduledoc """
+  Documentation for PrivCheck.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PrivCheck.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/priv_check_test.exs
@@ -0,0 +1,8 @@
+defmodule PrivCheckTest do
+  use ExUnit.Case
+  doctest PrivCheck
+
+  test "greets the world" do
+    assert PrivCheck.hello() == :world
+  end
+end
