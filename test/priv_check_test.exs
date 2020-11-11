ba4228881edbf16ac61b0e006537de517c7f6f06
Jason Axelson <jason.axelson@gmail.com>
2020-11-11 07:29:14 -1000

    Update meta test to latest version

--- test/priv_check_test.exs
+++ test/priv_check_test.exs
@@ -1,12 +1,4 @@
 defmodule PrivCheckTest do
   use ExUnit.Case, async: true
   doctest PrivCheck
-
-  test "README.md version is up to date" do
-    app = :priv_check
-    app_version = Application.spec(app, :vsn) |> to_string()
-    readme = File.read!("README.md")
-    [_, readme_version] = Regex.run(~r/{:#{app}, "(.+)".*}/, readme)
-    assert Version.match?(app_version, readme_version)
-  end
 end
